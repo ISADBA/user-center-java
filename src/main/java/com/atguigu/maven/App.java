3f9aa27fb0cad30040bc1f8635400afa1fe8fb6a
fenghao <chinafenghao@hotmail.com>
2023-10-21 14:27:41 +0800

    提供一个Calculate的类给user-center-web使用

--- src/main/java/com/atguigu/maven/App.java
+++ src/main/java/com/atguigu/maven/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Calculate calculate = new Calculate();
+        int addRes = calculate.add(3, 5);
+        System.out.println("3 + 5 = " + addRes);
     }
 }
---
+++ src/main/java/com/atguigu/maven/Calculate.java
@@ -0,0 +1,21 @@
+package com.atguigu.maven;
+
+/**
+ * ClassName: Calculate
+ * Package: com.atguigu.maven
+ * Description:
+ *
+ * @Author: fenghao
+ * @Create 2023/10/21 11:33
+ * @Version 1.0
+ */
+public class Calculate {
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int sub(int a, int b){
+            return a - b;
+        }
+}
